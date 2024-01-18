36d7444a3513b08d718e22c06dd057be3bcdf64c
David Hermosilla Duro <dhermosilla@sice-outsourcing.com>
2024-01-18 10:01:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Examples of three different ways of managing react forms
+
+Three different ways of managing react forms examples w/error managing (state (w/custom input hook and component), refs and formData). Free to copy, modify and use.
+
+## Login
+
+<!-- ![Quiz-1](https://github.com/davidtheweb-dev/quiz-game/assets/71373942/c79a2aaf-7a36-4c29-b28e-8e319483a80d) -->
+
+## Signup
+
+<!-- ![Quiz-2](https://github.com/davidtheweb-dev/quiz-game/assets/71373942/ad86060d-3487-412b-aa39-5bb6f2ff739d) -->
---
+++ src/hooks/useInput.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+export function useInput(defaultValue, validationFn) {
+  const [enteredValue, setEnteredValue] = useState(defaultValue);
+  const [didEdit, setDidEdit] = useState(false);
+
+  const valueIsValid = validationFn(enteredValue);
+
+  const handleInputChange = (event) => {
+    setEnteredValue(event.target.value);
+    setDidEdit(false);
+  };
+
+  const handleInputBlur = () => {
+    setDidEdit(true);
+  };
+
+  const handleReset = () => {
+    setEnteredValue(defaultValue);
+    setDidEdit(false);
+  };
+
+  return {
+    value: enteredValue,
+    hasError: didEdit && !valueIsValid,
+    handleInputChange,
+    handleInputBlur,
+    handleReset,
+  };
+}
